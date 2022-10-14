edabf4615e2b015fdeecc70c3d8d59552906078f
Sander Wegdam <sanderwegdam@gmail.com>
2022-10-14 15:19:58 +0200

    Updates

---
+++ precache-manifest.3df9c64140f5d33ae1af0dab0fabc946.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6121e935b4e15adeace4bc8f29050708",
+    "url": "/foto-favorieten-app/index.html"
+  },
+  {
+    "revision": "e192fde61008c8047f4c",
+    "url": "/foto-favorieten-app/static/css/main.ed5a4474.chunk.css"
+  },
+  {
+    "revision": "3580cb8d45e62ef48de9",
+    "url": "/foto-favorieten-app/static/js/2.1b19d11c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/foto-favorieten-app/static/js/2.1b19d11c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e192fde61008c8047f4c",
+    "url": "/foto-favorieten-app/static/js/main.5c0ef0be.chunk.js"
+  },
+  {
+    "revision": "fee73f973265ccbb17b0",
+    "url": "/foto-favorieten-app/static/js/runtime-main.c97b49d4.js"
+  }
+]);
